d49bcc566af6c57014bff6efd226ad8b7a528bbf
Dave Moore <dmoore19@gmail.com>
2020-07-23 14:40:40 -0500

    dammit

--- worker-python/app.py
+++ worker-python/app.py
@@ -23,7 +23,7 @@ def connect_postgres():
       print ("connecting to the DB") 
       #conn = psycopg2.connect("host=db user=postgres password=dbp host=172.30.114.217")
       #conn = psycopg2.connect ("host={} dbname={} user={} password={}".format("sample-app", "postgres", "dave", "dave") )
-      conn = psycopg2.connect ("host={} dbname={} user={} password={}".format("new-postgresql", "postgres", db_user, db_pass)
+      conn = psycopg2.connect ("host={} dbname={} user={} password={}".format("new-postgresql", "postgres", db_user, db_pass))
       print ("Successfully connected to PostGres")
       
       cursor = conn.cursor()
