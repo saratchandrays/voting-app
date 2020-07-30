46430ece6f0079d25c01d402d1ab2253c47bacd4
Dave Moore <dmoore19@gmail.com>
2020-07-29 20:41:45 -0500

    updated

--- vote/app.py
+++ vote/app.py
@@ -7,8 +7,6 @@ import json
 
 option_a = os.getenv('OPTION_A', "Python")
 option_b = os.getenv('OPTION_B', "Node.js")
-option_c = os.getenv('OPTION_C', "Java")
-option_d = os.getenv('OPTION_D', "Go")
 hostname = socket.gethostname()
 
 # may have to have env variable for Z
@@ -43,8 +41,6 @@ def hello():
         'index.html',
         option_a=option_a,
         option_b=option_b,
-        option_c=option_c,
-        option_d=option_d,
         hostname=hostname,
         vote=vote,
     ))
