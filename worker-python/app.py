91fc66d1c7d3548697856731d6c24d180df65e26
Dave Moore <dmoore19@gmail.com>
2020-07-29 22:38:44 -0500

    updated

--- worker-python/app.py
+++ worker-python/app.py
@@ -10,9 +10,9 @@ def get_redis():
    print ("connecting to redis!") 
    if (os.getenv('OS_ENV') == "Z"):
       print ("Connecting to Redis using Z connection string")
-      g.redis = Redis(host="new-redis", db=0, socket_timeout=5)  # on Z 
+      redis_conn = Redis(host="new-redis", db=0, socket_timeout=5)  # on Z 
    else: 
-      g.redis = Redis(host="new_redis", db=0, socket_timeout=5, password=os.getenv('redispasswd', "password"))
+      redis_conn = Redis(host="new_redis", db=0, socket_timeout=5, password=os.getenv('redispasswd', "password"))
       print ("Connecting to Redis using x86 connection string")
    print ("connected to redis!") 
    return redis_conn
