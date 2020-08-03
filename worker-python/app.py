695dd63ed0b8e7400cfb1914d673be95e96582a2
saratchandrays <62112492+saratchandrays@users.noreply.github.com>
2020-08-03 16:52:33 -0400

    Update app.py

--- worker-python/app.py
+++ worker-python/app.py
@@ -28,13 +28,14 @@ def connect_postgres():
       print ("connecting to the DB") 
       #conn = psycopg2.connect("host=db user=postgres password=dbp host=172.30.114.217")
       #conn = psycopg2.connect ("host={} dbname={} user={} password={}".format("sample-app", "postgres", "dave", "dave") )
-      conn = psycopg2.connect ("host={} dbname={} user={} password={}".format("new-postgresql", "db", "dave", "dave"))
+      conn = psycopg2.connect ("host={} dbname={} user={} password={}".format("new-postgresql", "postgres", "dave", "dave"))
       print ("Successfully connected to PostGres")
       
       cursor = conn.cursor()
       sqlCreateTable = "CREATE TABLE IF NOT EXISTS votes (id VARCHAR(255) NOT NULL, vote VARCHAR(255) NOT NULL);"
       cursor.execute(sqlCreateTable)
       print ("votes table created") 
+      conn.commit()
       cursor.close()
       return conn 
 
