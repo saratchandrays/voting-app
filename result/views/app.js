be9b0e382bc14d1813f0730fc15cb2b418df1322
saratchandrays <62112492+saratchandrays@users.noreply.github.com>
2020-08-04 11:01:38 -0400

    Update app.js

--- result/views/app.js
+++ result/views/app.js
@@ -40,7 +40,7 @@ function getPercentages(a, b) {
   var result = {};
 
   if (a + b > 0) {
-    result.b = Math.round(a / (a + b) * 100);
+    result.b = Math.round(b / (a + b) * 100);
     result.a = 100 - result.b;
   } else {
     result.a = result.b = 50;
