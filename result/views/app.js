a107edda96a72726e59369adecebc2b0c8972c00
saratchandrays <62112492+saratchandrays@users.noreply.github.com>
2020-08-04 12:08:27 -0400

    Update app.js

--- result/views/app.js
+++ result/views/app.js
@@ -11,8 +11,8 @@ app.controller('statsCtrl', function($scope){
   var updateScores = function(){
     socket.on('scores', function (json) {
        data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+       var a = parseInt(data.b || 0);
+       var b = parseInt(data.a || 0);
 
        var percentages = getPercentages(a, b);
 
